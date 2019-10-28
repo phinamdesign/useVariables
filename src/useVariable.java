f05d39d416d1299592e385916d76057217c1fb82
Phi Nam <phinamhd1992@gmail.com>
2019-10-28 14:23:15 +0700

    add useVariable

---
+++ src/useVariable.java
@@ -0,0 +1,16 @@
+public class useVariable {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha noi";
+        System.out.println("i la: "+i);
+        System.out.println("f la: "+f);
+        System.out.println("d la: "+d);
+        System.out.println("b la: "+b);
+        System.out.println("c la: "+c);
+        System.out.println("s la: "+s);
+    }
+}
